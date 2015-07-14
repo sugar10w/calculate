11abeb9e7c30e7e10ec4a86e65ec21af9c5fa143
sugar10w <sugar10w@163.com>
2015-07-14 23:08:08 +0800

    a bit Chinese notes

--- calculate.h
+++ calculate.h
@@ -4,12 +4,13 @@
 
 typedef struct node
 {
-	double k,xx;
+	double k,xx;	//其中k为系数，xx为次数
 	node* next;
 	node() { k=0; xx=0; next=NULL; }
-} KX;
+} KX;	//定义KX为多项式，利用链表存储；
 
-typedef KX (*OPR)(KX,KX);
+typedef KX (*OPR)(KX,KX);	//定义OPR为二元多项式运算符
+
 OPR GetOpr(char c);
 
 void show(KX poly[],char opr[],int braket[],int top);
